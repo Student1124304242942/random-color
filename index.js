de0fc8247617cf5450e509bc3a92ed54703e5ff6
Yunus936 <pulat2007@mail.ru>
2024-09-11 21:26:01 +0500

    second commit

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-let colors = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C",
-"D","E", "F"];
+let colors = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 const btn = document.getElementById('nav');
 const color = document.querySelector('.color');
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function() {
     let hexcolor = '#';
-    for(i = 0; i < 6; i++){
+    let btnColor = '#';
+ 
+    for(let i = 0; i < 6; i++) {
         hexcolor += colors[getRandom()];
     }
 
-    color.textContent  = hexcolor;
+    for(let i = 0; i < 6; i++) {
+        btnColor += colors[getRandom()];
+    }
+    color.textContent = hexcolor;
     document.body.style.backgroundColor = hexcolor;
-})
+    btn.style.backgroundColor = btnColor;  
+});
 
-function getRandom (){
+function getRandom() {
     return Math.floor(Math.random() * colors.length);
 }
